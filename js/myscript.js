2ccda9a38a68b1b317c39af497af5b049e8406cf
Valeria Nazarova <alira.vn@gmail.com>
2019-07-26 17:00:26 +0200

    create function to add event listener on Like-button

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,15 @@
-console.log("Hello");
-console.log(movies[1].movieName);
+// testing connection between html / js / json files
+// console.log("Hello");
+// console.log(movies[1].movieName);
+var button = document.getElementsByClassName("like-btn"); //will be an array
+//console.log(button);
 
 for (var i = 0; i < movies.length; i++) {
-	document.getElementById("article-container").innerHTML += `<article id="${i}"><img class="article-img" src="${movies[i].moviePoster}" alt="moviePoster from JSON"><div class="article-text"><h3>${movies[i].movieName}</h3><p>${movies[i].movieInfo}</p><div class="like-box"><button class="like-btn"><span>Like</span><img src="img/like_green.png"></button><div class="like-counter">${movies[i].movieLikes}</div></div>
+	document.getElementById("article-container").innerHTML += `<article id="movie${i}"><img class="article-img" src="${movies[i].moviePoster}" alt="moviePoster from JSON"><div class="article-text"><h3>${movies[i].movieName}</h3><p>${movies[i].movieInfo}</p><div class="like-box"><button id="${i}"class="like-btn"><span>Like</span><img src="img/like_green.png"></button><div class="like-counter">${movies[i].movieLikes}</div></div>
 </article>`;
 }
+
+for (var i = 0; i < button.length; i++) {
+	button[i].addEventListener("click", function(){addLike(this.getAttribute("id"))}, false);
+}
+
