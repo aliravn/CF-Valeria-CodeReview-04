2707e1e219ae9e568c8cc6257197d94be3e3f615
Valeria Nazarova <alira.vn@gmail.com>
2019-07-26 17:49:44 +0200

    create function to show increased number in green circle

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,12 @@
 // testing connection between html / js / json files
 // console.log("Hello");
-console.log(movies[1].movieLikes);
-console.log(typeof movies[1].movieLikes);
+// console.log(movies[1].movieLikes);
+// console.log(typeof movies[1].movieLikes);
 var button = document.getElementsByClassName("like-btn"); //will be an array
 //console.log(button);
 
 for (var i = 0; i < movies.length; i++) {
-	document.getElementById("article-container").innerHTML += `<article id="movie${i}"><img class="article-img" src="${movies[i].moviePoster}" alt="moviePoster from JSON"><div class="article-text"><h3>${movies[i].movieName}</h3><p>${movies[i].movieInfo}</p><div class="like-box"><button id="${i}"class="like-btn"><span>Like</span><img src="img/like_green.png"></button><div class="like-counter">${movies[i].movieLikes}</div></div>
+	document.getElementById("article-container").innerHTML += `<article><img class="article-img" src="${movies[i].moviePoster}" alt="moviePoster from JSON"><div class="article-text"><h3>${movies[i].movieName}</h3><p>${movies[i].movieInfo}</p><div class="like-box"><button id="${i}"class="like-btn"><span>Like</span><img src="img/like_green.png"></button><div id="like${i}" class="like-counter">${movies[i].movieLikes}</div></div>
 </article>`;
 }
 
@@ -18,4 +18,7 @@ function addLike(i){
 	console.log("hello from button" + i);
 	movies[i].movieLikes += 1;
 	console.log(movies[i].movieLikes);
+	var likesCircle = document.getElementById(`like${i}`);
+	console.log(likesCircle);
+	likesCircle.innerHTML = `${movies[i].movieLikes}`;
 }
