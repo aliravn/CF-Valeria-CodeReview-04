564740863c85a58b930853e708a2e2f281a7a8b2
Valeria Nazarova <alira.vn@gmail.com>
2019-07-26 22:36:33 +0200

    update json file properties names for readability,add eventListener on sortButton

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 for (var i = 0; i < movies.length; i++) {
-	document.getElementById("article-container").innerHTML += `<article><img class="article-img" src="${movies[i].moviePoster}" alt="moviePoster from JSON"><div class="article-text"><h3>${movies[i].movieName}</h3><p>${movies[i].movieInfo}</p><div class="like-box"><button id="${i}"class="like-btn"><span>Like</span><img src="img/like_green.png"></button><div id="like${i}" class="like-counter">${movies[i].movieLikes}</div></div></article>`;
+	document.getElementById("article-container").innerHTML += `<article><img class="article-img" src="${movies[i].poster}" alt="moviePoster from JSON"><div class="article-text"><h3>${movies[i].name}</h3><p>${movies[i].info}</p><div class="like-box"><button id="${i}"class="like-btn"><span>Like</span><img src="img/like_green.png"></button><div id="like${i}" class="like-counter">${movies[i].likes}</div></div></article>`;
 }
 
 var button = document.getElementsByClassName("like-btn"); //will be an array
@@ -10,12 +10,46 @@ for (var i = 0; i < button.length; i++) {
 
 function addLike(i){
 	// console.log("hello from button " + i); - testing if [i] is transmitted / OK
-	movies[i].movieLikes += 1; // increases number of likes after each button click
+	movies[i].likes += 1; // increases number of likes after each button click
 	// console.log(movies[i].movieLikes); // testing if number increased / OK
-	var likesCircle = document.getElementById(`like${i}`); // var to get the "green circle"
+	var likesCircle = document.getElementById(`like${i}`); 
 	// console.log(likesCircle); // test if eleent is reached / OK
-	likesCircle.innerHTML = `${movies[i].movieLikes}`; // changes the value displayed in green circle
+	likesCircle.innerHTML = `${movies[i].likes}`; // changes the value displayed in green circle
 }
+
+var sortButton = document.getElementById("sort");
+sortButton.addEventListener("click", sortMovies, false);
+
+function sortMovies(multidimArr_with_i_and_movies_i_movieLikes) {
+	console.log("hello from sort function");
+}
+
+
+
+
+
+
+// function ItemConstructor(name, price, id) {
+//     this.likes = likes;
+//     this.id = id;
+// }
+
+// function addToCart(i) {
+//     var cartItem = new ItemConstructor(products[i].name, products[i].price, i);
+//     cart.push(cartItem);
+//     console.log(cart);
+//     largeoutput.innerHTML += `<div id"${i}"><img src="${products[i].pic}"><p>${cart[i].name}</p><p>Price: ${cart[i].price} </p></div>`;
+    // alert("Product has been added. Please view your shopping cart for details");
+// }
+
+// function sortMovies(multidimArr_with_i_and_movies_i_movieLikes) {
+// 		var sortingList = [];
+// 		for (var i = 0, i < movies.length, i++) {
+// 			push.
+// 		}
+// }
+
+
 
 // TESTING AREA
 // console.log("Hello"); - testing connection between html / js files
